5a3aadf1e3b190fcab52ad75607c2929edd0215b
Pierre Seguin <pierre@orohealth.me>
2023-10-21 23:48:54 -0400

    feat(app): added accepted/visible characters for passwords

---
+++ src/models.rs
@@ -0,0 +1,4 @@
+pub const SYMBOLS: &str = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+pub const LOWERCASE: &str = "abcdefghijklmnopqrstuvwxyz";
+pub const UPPERCASE: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+pub const DIGITS: &str = "0123456789";
